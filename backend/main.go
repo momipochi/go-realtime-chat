5adaf95d754a097166e1d809a7f80f970549e061
unknown <k25650727@gmail.com>
2024-05-28 16:23:21 +0200

    multi client support

--- backend/main.go
+++ backend/main.go
@@ -7,21 +7,31 @@ import (
 )
 
 // define our WebSocket endpoint
-func serveWs(w http.ResponseWriter, r *http.Request) {
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
 	ws, err := websocket.Upgrade(w, r)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Fprintf(w, "%+v\n", err)
 	}
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+	client := &websocket.Client{
+		Conn: ws,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
 }
 
 func setupRoutes() {
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 }
 
 func main() {
-	fmt.Println("Chat App v0.01")
+	fmt.Println("Multi client chat App v0.01")
 	setupRoutes()
 	http.ListenAndServe(":8080", nil)
 }
--- backend/pkg/websocket/pool.go
+++ backend/pkg/websocket/pool.go
@@ -1,5 +1,7 @@
 package websocket
 
+import "fmt"
+
 type Pool struct {
 	Register   chan *Client
 	Unregister chan *Client
@@ -13,5 +15,36 @@ func NewPool() *Pool {
 		Unregister: make(chan *Client),
 		Clients:    make(map[*Client]bool),
 		Broadcast:  make(chan Message),
+	}
+}
+
+func (pool *Pool) Start() {
+	for {
+		select {
+		case client := <-pool.Register:
+			pool.Clients[client] = true
+			fmt.Println("Size of the connection pool: ", len(pool.Clients))
+			for client, _ := range pool.Clients {
+				fmt.Println(client)
+				client.Conn.WriteJSON(Message{Type: 1, Body: "New user joined..."})
+			}
+			break
+		case client := <-pool.Unregister:
+			delete(pool.Clients, client)
+			fmt.Println("Size of connection pool: ", len(pool.Clients))
+			for client, _ := range pool.Clients {
+				client.Conn.WriteJSON(Message{Type: 1, Body: "User disconnected..."})
+			}
+			break
+		case message := <-pool.Broadcast:
+			fmt.Println("Sending message to all clients in pool")
+			for client, _ := range pool.Clients {
+				if err := client.Conn.WriteJSON(message); err != nil {
+					fmt.Println(err)
+					return
+				}
+			}
+		}
+
 	}
 }
